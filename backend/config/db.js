a148a92ece3526c75f016aea6f2e5bb74a9b76b9
Keegan <keegan.adams15@gmail.com>
2021-03-30 18:21:13 -0500

    update

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,15 +1,15 @@
-export const db = {
-  host: "localhost",
-  user: "root",
-  password: "password",
-  database: "jabber",
-};
-
 // export const db = {
-//   host: "us-cdbr-east-03.cleardb.com",
-//   user: "b1ac3aa592391f",
-//   password: "b6d25959",
-//   database: "heroku_f2ca863b53af3f6",
+//   host: "localhost",
+//   user: "root",
+//   password: "password",
+//   database: "jabber",
 // };
+
+export const db = {
+  host: "us-cdbr-east-03.cleardb.com",
+  user: "b1ac3aa592391f",
+  password: "b6d25959",
+  database: "heroku_f2ca863b53af3f6",
+};
 
 // b1ac3aa592391f:b6d25959@us-cdbr-east-03.cleardb.com/heroku_f2ca863b53af3f6?reconnect=true
