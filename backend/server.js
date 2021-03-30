53e87bd70e1e324a607126f884b0500680c45a32
Keegan <keegan.adams15@gmail.com>
2021-03-30 16:55:12 -0500

    prep for deploy to heroku

--- backend/server.js
+++ backend/server.js
@@ -24,13 +24,21 @@ app.use("/api/topics", topicRoutes);
 app.use("/api/comments", commentRoutes);
 app.use(errorHandler);
 
-app.get("/", (req, res) => {
-  res.send("API is running...");
-});
-
 const __dirname = path.resolve();
 
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running...");
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
